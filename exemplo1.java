9194f17fadd0bd6c087965cc2c1412b7d593f468
takeshibruno <bruno@rodpheus.net>
2024-09-19 20:08:29 -0300

    teste3

--- exemplo1.java
+++ exemplo1.java
@@ -2,6 +2,8 @@ public class exemplo1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Alteração 2");
+
+        System.out.println("alteração 3");
     }
     
 }
